885142c7fe94e7ee3ad6e781ed4bee91bc33cd93
LuizFelipePLO <felipe08074@gmail.com>
2023-05-26 20:16:33 -0300

    update student model

--- src/models/StudentModel.js
+++ src/models/StudentModel.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
+const studentData = require('./Student')
 
 const pool = new Pool({
-  host: 'backend-g7-db-1',
+  host: 'localhost',
   port: 5432,
   user: 'root',
   password: 'root',
@@ -11,31 +12,59 @@ const pool = new Pool({
 class StudentModel {
   static async createStudent(student) {
     // pool.query() / pool.execute()
+    try {
+      const query = `INSERT INTO aluno (column1, column2, ...) VALUES ($1, $2, ...)`;
+      const values = [studentData]; 
+      const result = await pool.query(query, values);
+      return result.rows;
+    } catch (error) {
+      throw new Error(`Erro ao listar estudantes: ${error.message}`);
+    }
   }
 
   static async updateStudent(id, newData) {
+    try {
+      const query = `UPDATE students SET * = ${newData} WHERE aluno_id = ${id}`;
+      const result = await pool.query(query, [newData, id]);
+      return result.rows;
+    } catch (error) {
+      throw new Error(`Erro ao listar estudantes: ${error.message}`);
+    }
   }
 
-  static async updateStudentByAttribute(id, newData) {
+  static async updateStudentByAttribute(id, attribute, value, newData) {
+    try {
+      const query = `UPDATE students SET ${attribute} = ${newData} WHERE aludo_id = ${id} AND ${attribute} = ${value}`;
+      const result = await pool.query(query, [newData, value]);
+      return result.rows;
+    } catch (error) {
+      throw new Error(`Erro ao atualizar estudante por atributo: ${error.message}`);
+    }
   }
 
   static async deleteStudent(id) {
+    try {
+      const query = `DELETE * FROM students WHERE aluno_id = ${id}`;
+      const result = await pool.query(query, [id]);
+      return result.rows;
+    } catch (error) {
+      throw new Error(`Erro ao listar estudantes: ${error.message}`);
+    }
   }
 
   static async listStudentById(id) {
     try {
-      const query = 'SELECT * FROM aluno WHERE id = $1';
-      const values = [id];
-      const result = await pool.query(query, values);
-      return result.rows[0];
+      const query = `SELECT * FROM students WHERE aluno_id = ${id}`;
+      const result = await pool.query(query, [id]);
+      return result.rows;
     } catch (error) {
-      throw new Error(`Erro ao obter estudante por ID: ${error.message}`);
+      throw new Error(`Erro ao listar estudantes: ${error.message}`);
     }
   }
 
   static async listStudents() {
     try {
-      const query = 'SELECT * FROM aluno';
+      const query = 'SELECT * FROM students';
       const result = await pool.query(query);
       return result.rows;
     } catch (error) {
