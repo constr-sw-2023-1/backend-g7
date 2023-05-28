b3a10700f22f0e8da59443f3e7098d656554f290
bruno <brunoramos013@gmail.com>
2023-05-28 14:29:53 -0300

    clean index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-//const cors = require('./src/utils/cors');
 const routes = require('./src/routes');
 
 app.use(express.json());
-//app.use(cors);
 app.use('/', routes);
 
 app.listen(8080);
