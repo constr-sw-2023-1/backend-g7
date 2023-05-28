cb32eef8695adf8514985a13139229c1c1bf8e70
bruno <brunoramos013@gmail.com>
2023-05-28 14:31:52 -0300

    Adjust controller

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -1,14 +1,10 @@
-const request = require('request');
-
-require('dotenv').config();
-const config = require('../config/config');
 const StudentModel = require('../models/StudentModel');
 
 async function createStudent (req, res) {
     try {
         const student = await StudentModel.createStudent(req.body);
         res.status(201).send({
-            message: "User created successfully",
+            message: "student created successfully",
             data: student
         });
     } catch (error) {
@@ -21,7 +17,7 @@ function deleteStudent(req, res) {
     try {
         StudentModel.deleteStudent(req.params.id);
         res.status(200).send({
-            message: "User deleted successfully",
+            message: "student deleted successfully",
         });
     } catch (error) {
         console.error(error);
@@ -29,78 +25,28 @@ function deleteStudent(req, res) {
     }
 }
 
-function updateStudent(req, res) {
-    const options = {
-        url: `${config.baseApiUrl}/students/${req.params.id}`,
-        headers: {
-            Authorization: req.headers.authorization,
-            'Content-Type': 'application/json'
-        },
-        json: {
-            "firstName": "Jorge",
-        }
-    };
-    request.put(options, (error, response, body) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).send({ error: 'Internal Server Error' });
-        }
-        try {
-            if(response.statusCode === 200){
-            console.log(`Status code: ${response.statusCode}`);
-            console.log(`Response body: ${body}`);
-            res.status(200).send({
-                message: "User updated successfully" });
-            } else if (response.statusCode === 404){
-                res.status(404).send({ error: 'User Not Found' });
-            } else {
-                throw new Error(`Unexpected response status code: ${response.statusCode}`);
-            }
-        } catch (error) {
-            console.error(error);
-            res.status(500).send({ error: 'Internal Server Error' });
-        }
-    });
+async function updateStudent(req, res) {
+    const { id } = req.params;
+    const newData = req.body;
+
+    try {
+        const updatedStudent = await StudentModel.updateStudent(id, newData);
+        res.json(updatedStudent);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
 
-function updateStudentByAttribute(req, res) {
-    const options = {
-        url: `${config.baseApiUrl}/students/${req.params.id}/student-data`,
-        headers: {
-            Authorization: req.headers.authorization,
-            'Content-Type': 'application/json'
-        },
-        body: {
-            type: 'password',
-            temporary: false,
-            value: req.body.password
-        },
-        json: true
-    };
+async function updateStudentByAttribute(req, res) {
+    const { id } = req.params;
+    const newData = req.body;
 
-    request.patch(options, (error, response, body) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).send({ error: 'Internal Server Error' });
-        }
-        console.log('Response:', response.statusCode, body);
-        try{
-            if (response.statusCode === 200) {
-                res.status(200).send({
-                    message: "User password updated successfully"
-                });
-            } else if (response.statusCode === 404) {
-                res.status(404).send({
-                    message: "Requested password not found"
-                });
-            } else {
-                throw new Error(`Unexpected response status code: ${response.statusCode}`);
-            }
-        }  catch (error) {
-            console.error(error);
-            res.status(500).send({ error: 'Internal Server Error' });
-        }
-    });
+    try {
+        const updatedStudent = await StudentModel.updateStudentByAttribute(id, newData);
+        res.json(updatedStudent);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
 
 async function listStudents(req, res) {
@@ -127,7 +73,7 @@ async function listStudentById(req, res) {
     
             return res.status(200).send({
                 message: 'Student listed successfully',
-                user: student,
+                student: student,
             });
         } catch (error) {
             console.error('Error:', error);
@@ -137,9 +83,9 @@ async function listStudentById(req, res) {
 
 async function listStudentByQueryString(req, res) {
     try {
-        const filters = req.query; // Obtém os parâmetros de filtro da requisição
+        const filters = req.query;
+        const result = await StudentModel.listStudentByQueryString(filters);
 
-        const result = await StudentModel.listStudentByQueryString(filters); // Chama a função que faz a consulta no banco de dados
         res.json(result);
     } catch (error) {
         console.error('Error:', error);
