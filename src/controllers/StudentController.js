b94b460affd38121fe5e7dc3ac1ad83dd9906f14
LuizFelipePLO <felipe08074@gmail.com>
2023-05-29 10:28:08 -0300

    Update controller with error treatment

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -1,7 +1,7 @@
 const StudentModel = require('../models/StudentModel');
 const ApiError = require('../errors/ApiError');
 
-async function createStudent (req, res) {
+async function createStudent (req, res, next) {
     try {
         const student = await StudentModel.createStudent(req.body);
         res.status(201).send({
@@ -9,24 +9,37 @@ async function createStudent (req, res) {
             data: student
         });
     } catch (error) {
-        console.error(error);
-        res.status(500).send({ error: 'Internal Server Error' });
+        if(res.status(400)){
+            next(ApiError.badRequest('Invalid input'));
+            return;
+        } else if (res.status(500)){
+            next(ApiError.internalError('Internal Server Error'));
+            return;
+        }
     }
 }
 
-function deleteStudent(req, res) {
+function deleteStudent(req, res, next) {
     try {
         StudentModel.deleteStudent(req.params.id);
         res.status(200).send({
             message: "student deleted successfully",
         });
     } catch (error) {
-        console.error(error);
-        res.status(500).send({ error: 'Internal Server Error' });
+        if(res.status(400)){
+            next(ApiError.badRequest('Invalid input. Check your URL'));
+            return;
+        } else if(res.status(404)){
+            next(ApiError.notFound('Student not found'));
+            return;
+        } else if (res.status(500)){
+            next(ApiError.internalError('Internal Server Error'));
+            return;
+        }
     }
 }
 
-async function updateStudent(req, res) {
+async function updateStudent(req, res, next) {
     const { id } = req.params;
     const newData = req.body;
 
@@ -34,7 +47,16 @@ async function updateStudent(req, res) {
         const updatedStudent = await StudentModel.updateStudent(id, newData);
         res.json(updatedStudent);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        if(res.status(400)){
+            next(ApiError.badRequest('Invalid input. Check your URL'));
+            return;
+        } else if(res.status(404)){
+            next(ApiError.notFound('Student not found'));
+            return;
+        } else if (res.status(500)){
+            next(ApiError.internalError('Internal Server Error'));
+            return;
+        }
     }
 }
 
@@ -46,8 +68,14 @@ async function updateStudentByAttribute(req, res, next) {
         const updatedStudent = await StudentModel.updateStudentByAttribute(id, newData);
         res.json(updatedStudent);
     } catch (error) {
-        if(!updateStudent){
+        if(res.status(400)){
+            next(ApiError.badRequest('Invalid input. Check your URL'));
+            return;
+        } else if(res.status(404)){
             next(ApiError.notFound('Student not found'));
+            return;
+        } else if (res.status(500)){
+            next(ApiError.internalError('Internal Server Error'));
             return;
         }
     }
@@ -62,14 +90,20 @@ async function listStudents(req, res, next) {
             students: students
         });
     } catch (error) {
-        if(!updateStudent){
+        if(res.status(400)){
+            next(ApiError.badRequest('Invalid input. Check your URL'));
+            return;
+        } else if(res.status(404)){
             next(ApiError.notFound('Student not found'));
+            return;
+        } else if (res.status(500)){
+            next(ApiError.internalError('Internal Server Error'));
             return;
         }
     }
 }
 
-async function listStudentById(req, res) {
+async function listStudentById(req, res, next) {
         try {
             const student = await StudentModel.listStudentById(req.params.id);
     
@@ -82,21 +116,37 @@ async function listStudentById(req, res) {
                 student: student,
             });
         } catch (error) {
-            console.error('Error:', error);
-            return res.status(500).send({ error: 'Internal Server Error' });
+            if(res.status(400)){
+                next(ApiError.badRequest('Invalid input. Check your URL'));
+                return;
+            } else if(res.status(404)){
+                next(ApiError.notFound('Student not found'));
+                return;
+            } else if (res.status(500)){
+                next(ApiError.internalError('Internal Server Error'));
+                return;
+            }
         }
     }
 
-async function listStudentByQueryString(req, res) {
+async function listStudentByQueryString(req, res, next) {
     try {
         const filters = req.query;
         const result = await StudentModel.listStudentByQueryString(filters);
 
         res.json(result);
     } catch (error) {
-        console.error('Error:', error);
-        return res.status(500).send({ error: 'Internal Server Error' });
+        if(res.status(400)){
+            next(ApiError.badRequest('Invalid input. Check your URL'));
+            return;
+        } else if(res.status(404)){
+            next(ApiError.notFound('Student not found'));
+            return;
+        } else if (res.status(500)){
+            next(ApiError.internalError('Internal Server Error'));
+            return;
+        }
     }
 }
 
-module.exports = { createStudent, deleteStudent, updateStudent, updateStudentByAttribute, listStudents, listStudentById, listStudentByQueryString};
+module.exports = { createStudent, deleteStudent, updateStudent, updateStudentByAttribute, listStudents, listStudentById, listStudentByQueryString };
