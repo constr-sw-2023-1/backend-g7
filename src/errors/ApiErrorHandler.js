e5d89d50b412298bcfda32ef332e32bbf4d8376c
LuizFelipePLO <felipe08074@gmail.com>
2023-05-29 15:18:02 -0300

    update error handler treatment

---
+++ src/errors/ApiErrorHandler.js
@@ -0,0 +1,13 @@
+const ApiError = require('./ApiError');
+
+function apiErrorHandler(err, req, res, next) {
+    
+  if (err instanceof ApiError) {
+    res.status(err.code).json(err.message);
+    return;
+  }
+
+  res.status(500).json('Something went wrong...');
+}
+
+module.exports = apiErrorHandler;
